f4fd30a96e5a7458e7ddf20a1599b22cf4caf347
David Walker <dave@sitecoredave.com>
2023-02-23 12:43:33 -0600

    template path

--- azurerm/_smoketest_files/main.tf
+++ azurerm/_smoketest_files/main.tf
@@ -1,10 +1,10 @@
 module "local_file_debug" {
-  source   = "../../../templates/local/local_file"
+  source   = "../../local/local_file"
   content  = "timestamp=${timestamp()}"
   filename = "debug.txt"
 }
 
 module "write_files_debug" {
-  source = "../../../templates/local/write_files"
+  source = "../../local/write_files"
   files  = { "debug.txt" = "timestamp=${timestamp}" }
 }
