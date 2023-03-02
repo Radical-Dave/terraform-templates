56ea5d184b4df1be895bf59c9e1a8aff8bacf672
David Walker <dave@radicaldave.com>
2023-03-02 00:29:44 -0600

    Update providers.tf

--- azurerm/_smoketest_az/providers.tf
+++ azurerm/_smoketest_az/providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~>3.43"
     }
     random = {
       source  = "hashicorp/random"
@@ -16,8 +16,5 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "d4dcf2f0-f230-4785-ac04-a37ed6e439ee"
-  client_id       = "cdbc55e5-1f66-48dc-a69f-ee67dd1fa5d4"
-  client_secret   = "liGFjPWb.Ls7phPl_VNrx4zsA_xMTkD5mn"
-  tenant_id       = "e3c1b2e3-3961-4882-925a-94cf6bf2170a"
+
 }
